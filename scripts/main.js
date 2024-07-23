f2744e0aa7126301235bc918e0d195e3718dd266
Msebeyenjabulo <Mag24012@byui.edu>
2024-07-23 22:56:13 +0200

    web project

--- scripts/main.js
+++ scripts/main.js
@@ -82,3 +82,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+// Hamburger Menu
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('nav ul'); // Updated selector
+
+    hamburger.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+});
+
+
